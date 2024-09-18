f72d561dd608615303f8868f6835fb5484be251a
Rajoo yadav <rajooyadav8423@gmail.com>
2024-09-18 15:37:25 +0530

    create  Maflam Instructors component and Success Partners  component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+
+import MaflamInstructors from "../../Component/MaflamInstructors/MaflamInstructors";
 import Navbar from "../../Component/Navbar/Navbar";
+import SuccessPartners from "../../Component/SuccessPartners/SuccessPartners";
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
+    <MaflamInstructors></MaflamInstructors>
+    <SuccessPartners></SuccessPartners>
     </div>
   );
 };
