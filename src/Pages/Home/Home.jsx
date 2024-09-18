0dcc15a76de2579935b51187c39a2f15b4a314ac
lokendernpr <lokender@nprservices.in>
2024-09-18 12:25:14 +0530

    just pull

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import CourseGrid from "../../Component/CourseCategories/Course";
 
 const Home = () => {
   return (
     <div>
       <p>Home Page</p>
+      <CourseGrid></CourseGrid>
     </div>
   );
 };
