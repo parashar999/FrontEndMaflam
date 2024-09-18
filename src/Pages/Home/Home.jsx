00d460b72a583d310e09a4b36babad0b97738481
harshsharmanpr <harsh.sharma@nprservices.in>
2024-09-18 15:21:46 +0530

    adding HomeHero component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../../Component/Navbar/Navbar";
+import HomeHero from "../../Component/HomeHero/HomeHero";
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <HomeHero></HomeHero>
     </div>
   );
 };
