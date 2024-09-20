868c7fb1b4a16b5e7b23ac59280d6a0367907f39
Surjeet <surjeet.yadav@nprservices.in>
2024-09-20 11:34:10 +0530

    just pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
-
 import { Route, Routes } from "react-router-dom";
 import AboutUs from "./Pages/AboutUs/AboutUs";
 import Home from "./Pages/Home/Home";
 import Pricing from "./Pages/Pricing/pricing";
 import Fundamental from "./Component/FundamentalPackage/Fundamental";
 
-
-
-
-
 const App = () => {
   return (
     <div>
-  
-     <Routes>
-         {/* <Route path="/" element ={ <Home></Home>}/> */}
-       {/*  <Route path="/aboutus" element ={ <AboutUs></AboutUs>}/> */}
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/aboutus" element={<AboutUs></AboutUs>} />
         <Route path="/prc" element={<Pricing></Pricing>}></Route>
-       
-     </Routes>
+      </Routes>
     </div>
   );
 };
