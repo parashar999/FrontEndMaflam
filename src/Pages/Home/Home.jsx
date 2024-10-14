dac0a8ea0d35a6f9522e3278fc2ba7af6930b593
Rajoo yadav <rajooyadav8423@gmail.com>
2024-10-14 15:33:40 +0530

    Changes the homePage under the component  Show data in Arabic

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -48,7 +48,72 @@
 
 
 
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+// import Navbar from "../../Component/Navbar/Navbar";
+// import HomeHero from "../../Component/HomeHero/HomeHero";
+// import LearnMaflam from "../../Component/LearnMaflam/LearnMaflam";
+// import ViewCourses from "../../Component/ViewCourses/ViewCourses";
+// import CourseGrid from "../../Component/CourseCategories/Course";
+// import Fundamental from "../../Component/FundamentalPackage/Fundamental";
+// import Ourservices from "../../Component/Ourservices/Ourservices";
+// import SuccessPartners from "../../Component/SuccessPartners/SuccessPartners";
+// import LearnAbout from "../../Component/LearnAbout/LearnAbout";
+// import MaflamInstructors from "../../Component/MaflamInstructors/MaflamInstructors";
+// import HomeScreenCarousel from "../../Component/HomeScreenCarousel/HomeScreenCarousel";
+// import BlogContainer from "../../Component/Blogs/BlogContainer";
+// import HomeCommunityCarousel from "../../Component/HomeCummunityCarousel/HomeCommunityCarousel";
+// import CarouselCommunity from "../../Component/Community/CarouselCommunity";
+// import Faqs from "../../Component/Faqs/Faqs";
+// import Footer from "../../Component/Footer/Footer";
+// import Styles from "./Home.module.css";
+
+// const Home = () => {
+//   const [language, setLanguage] = useState("ar"); 
+//   const [direction, setDirection] = useState("rtl"); 
+
+
+//   useEffect(() => {
+//     if (language === "ar") {
+//       document.body.dir = "rtl"; 
+//       setDirection("rtl");
+//     } else {
+//       document.body.dir = "ltr"; 
+//       setDirection("ltr");
+//     }
+//   }, [language]);
+
+//   const toggleLanguage = () => {
+//     setLanguage((prevLanguage) => (prevLanguage === "en" ? "ar" : "en"));
+//   };
+
+//   return (
+//     <div className={Styles.Home}>
+//       <Navbar direction={direction} language={language} toggleLanguage={toggleLanguage} />
+//       {/* <Navbar /> */}
+//       <HomeHero direction={direction} language={language} />
+//       <LearnMaflam direction={direction} language={language} />
+//       <ViewCourses direction={direction} language={language} />
+//       <CourseGrid direction={direction} language={language} />
+//       <Fundamental direction={direction} language={language} />
+//       <Ourservices direction={direction} language={language} />
+//       <SuccessPartners direction={direction} language={language} />
+//       <LearnAbout direction={direction} language={language} />
+//       <MaflamInstructors direction={direction} language={language} />
+//       <HomeScreenCarousel direction={direction} language={language} />
+//       <BlogContainer direction={direction} language={language} />
+//       <HomeCommunityCarousel direction={direction} language={language} />
+//       <CarouselCommunity direction={direction} language={language} />
+//       <Faqs direction={direction} language={language} />
+//       <Footer direction={direction} language={language} />
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+
+
+import React from "react";
 import Navbar from "../../Component/Navbar/Navbar";
 import HomeHero from "../../Component/HomeHero/HomeHero";
 import LearnMaflam from "../../Component/LearnMaflam/LearnMaflam";
@@ -66,46 +131,30 @@ import CarouselCommunity from "../../Component/Community/CarouselCommunity";
 import Faqs from "../../Component/Faqs/Faqs";
 import Footer from "../../Component/Footer/Footer";
 import Styles from "./Home.module.css";
-
+import { LanguageProvider } from "../../Component/LanguageContext/LanguageContext";
+// import { LanguageProvider } from "../../context/LanguageContext"; 
 const Home = () => {
-  const [language, setLanguage] = useState("ar"); 
-  const [direction, setDirection] = useState("rtl"); 
-
-
-  useEffect(() => {
-    if (language === "ar") {
-      document.body.dir = "rtl"; 
-      setDirection("rtl");
-    } else {
-      document.body.dir = "ltr"; 
-      setDirection("ltr");
-    }
-  }, [language]);
-
-  const toggleLanguage = () => {
-    setLanguage((prevLanguage) => (prevLanguage === "en" ? "ar" : "en"));
-  };
-
   return (
-    <div className={Styles.Home}>
-      <Navbar direction={direction} language={language} toggleLanguage={toggleLanguage} />
-      {/* <Navbar /> */}
-      <HomeHero direction={direction} language={language} />
-      <LearnMaflam direction={direction} language={language} />
-      <ViewCourses direction={direction} language={language} />
-      <CourseGrid direction={direction} language={language} />
-      <Fundamental direction={direction} language={language} />
-      <Ourservices direction={direction} language={language} />
-      <SuccessPartners direction={direction} language={language} />
-      <LearnAbout direction={direction} language={language} />
-      <MaflamInstructors direction={direction} language={language} />
-      <HomeScreenCarousel direction={direction} language={language} />
-      <BlogContainer direction={direction} language={language} />
-      <HomeCommunityCarousel direction={direction} language={language} />
-      <CarouselCommunity direction={direction} language={language} />
-      <Faqs direction={direction} language={language} />
-      <Footer direction={direction} language={language} />
-    </div>
+    <LanguageProvider>
+      <div className={Styles.Home}>
+        <Navbar />
+        <HomeHero />
+        <LearnMaflam />
+        <ViewCourses />
+        <CourseGrid />
+        <Fundamental />
+        <Ourservices />
+        <SuccessPartners />
+        <LearnAbout />
+        <MaflamInstructors />
+        <HomeScreenCarousel />
+        <BlogContainer />
+        <HomeCommunityCarousel />
+        <CarouselCommunity />
+        <Faqs />
+        <Footer />
+      </div>
+    </LanguageProvider>
   );
 };
 
