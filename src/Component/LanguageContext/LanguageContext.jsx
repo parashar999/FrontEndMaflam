e2604e1743aee83cf7a87da03ae855c6baa13f6f
harshsharma72 <harsh.sharma@nprservices.in>
2024-10-14 17:12:33 +0530

    Adding arabic Api in HomeHero component

--- src/Component/LanguageContext/LanguageContext.jsx
+++ src/Component/LanguageContext/LanguageContext.jsx
@@ -1,4 +1,4 @@
-import  { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
 
 // Create the context
 export const LanguageContext = createContext();
