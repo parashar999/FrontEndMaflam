e7233fb06bd14936907e9fced2a99c8832e9d303
lokendernpr <lokender@nprservices.in>
2024-10-18 15:18:31 +0530

    SignUp component crate

---
+++ src/Pages/SignUpPage/SignUpPage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "../../Component/Navbar/Navbar";
+import Footer from "../../Component/Footer/Footer";
+import { LanguageProvider } from "../../Component/LanguageContext/LanguageContext";
+import SignUp from "../../Component/SignUp/SignUp";
+
+const SignUpPage  = () => {
+  return (
+    <div>
+   <LanguageProvider >
+    <Navbar></Navbar>
+        <SignUp></SignUp>
+    <Footer></Footer>
+   </LanguageProvider >
+
+    </div>
+  );
+};
+
+export default SignUpPage ;
