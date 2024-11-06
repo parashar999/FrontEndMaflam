de8b66feef6ee775ba3556a78699110077f7b674
harshsharmanpr <harsh.sharma@nprservices.in>
2024-11-06 12:55:24 +0530

    adding the gradient

--- src/Component/HomeHero/HomeHero.jsx
+++ src/Component/HomeHero/HomeHero.jsx
@@ -6,9 +6,9 @@ import { LanguageContext } from "../../Component/LanguageContext/LanguageContext
 
 const HomeHero = () => {
   const { homeScreenDetails, loading, error } = useContext(HomePageContext);
-  const { language} = useContext(LanguageContext);
-console.log(language);
-  const stylesSelected = language === "ar"?styles:stylesArabic;
+  const { language } = useContext(LanguageContext);
+  console.log(language);
+  const stylesSelected = language === "ar" ? styles : stylesArabic;
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error loading data</p>;
@@ -24,30 +24,30 @@ console.log(language);
 
   return (
     <div className={stylesSelected.herosection}>
-    <div className={stylesSelected.heroContainer}>
-      {videoUrl && (
-        <video
-          className={stylesSelected.backgroundVideo}
-          src={videoUrl}
-          autoPlay
-          muted
-          loop
-        />
-      )}
-      <div className={stylesSelected.overlay}></div>
-      <div className={stylesSelected.rightoverlay}> </div>
-      <div className={stylesSelected.leftoverlay}></div>
-      <div className={stylesSelected.bottomoverlay}> </div>
-      <div className={stylesSelected.content}>
-        <h1 className={stylesSelected.title} style={{ fontSize: 45 }}>
-          {title}
-        </h1>
-        <p className={stylesSelected.description}>{description}</p>
-         {/* {signUpText && (
+      <div className={stylesSelected.heroContainer}>
+        {videoUrl && (
+          <video
+            className={stylesSelected.backgroundVideo}
+            src={videoUrl}
+            autoPlay
+            muted
+            loop
+          />
+        )}
+        <div className={stylesSelected.overlay}></div>
+        <div className={stylesSelected.rightoverlay}> </div>
+        <div className={stylesSelected.leftoverlay}></div>
+        <div className={stylesSelected.bottomoverlay}></div>
+        <div className={stylesSelected.content}>
+          <h1 className={stylesSelected.title} style={{ fontSize: 45 }}>
+            {title}
+          </h1>
+          <p className={stylesSelected.description}>{description}</p>
+          {/* {signUpText && (
           <button className={styles.signUpButton}>{signUpText}</button>
         )}  */}
+        </div>
       </div>
-    </div>
     </div>
   );
 };
